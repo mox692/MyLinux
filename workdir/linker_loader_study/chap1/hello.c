1adc47d7f94fcaa2f3599111210bfdab0d21aec7
mox692 <55653825+mox692@users.noreply.github.com>
2021-05-17 23:21:22 +0900

    feat: linker進め

---
+++ workdir/linker_loader_study/chap1/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int a = 0;
+int b = 2;
+char st[15];
+int main() { 
+    char str[19];
+    printf("hello world\n"); }
---
+++ workdir/linker_loader_study/chap2/hello2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int a = 3;
+int main() { 
+    printf("input some number\n");
+    scanf("%d", &a);
+    printf("input is %d\n", a);
+}
